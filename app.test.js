a33795400b37b904373e4d47510c3f3e2307aca7
Techkny <bobadeprojects@gmail.com>
2019-04-17 00:52:22 +0100

    Wrote Test To Check If User has Been Added to Database

--- app.test.js
+++ app.test.js
@@ -3,31 +3,47 @@ const { Admin } = require("./app");
 const db = require("./database");
 
 test('Should Create New User with corresponding name "Shalom" ', function() {
-  let Shalom = new User("Shalom", "shalom@gmail.com", 12345);
+  var Shalom = new User("Shalom", "shalom@gmail.com", 12345);
   expect(Shalom.name).toBe("Shalom");
 });
 
 test('Should Create New User with corresponding email "shalom@gmail.com" ', function() {
-  let Dickson = new User("Dickson", "dickson@gmail.com", 12345);
+  var Dickson = new User("Dickson", "dickson@gmail.com", 12345);
   expect(Dickson.email).toBe("dickson@gmail.com");
 });
 
 test('Should Create New User with corresponding password "12345" ', function() {
-  let Uchenna = new User("Uchenna", "shalom@gmail.com", 12345);
+  var Uchenna = new User("Uchenna", "shalom@gmail.com", 12345);
   expect(Uchenna.password).toBe(12345);
 });
 
 test('Should Create New Admin with corresponding name "Adedayo" ', function() {
-  let Adedayo = new Admin("Adedayo", "samfeolu@gmail.com", 1234);
+  var Adedayo = new Admin("Adedayo", "samfeolu@gmail.com", 1234);
   expect(Adedayo.name).toBe("Adedayo");
 });
 
 test('Should Create New User with corresponding email "samfeolu@gmail.com" ', function() {
-  let sampleAdmin = new Admin("Adedayo", "samfeolu@gmail.com", 1234);
+  var sampleAdmin = new Admin("Adedayo", "samfeolu@gmail.com", 1234);
   expect(sampleAdmin.email).toBe("samfeolu@gmail.com");
 });
 
 test('Should Create New User with corresponding password "12345" ', function() {
-  let sampleAdmin1 = new Admin("Adedayo", "samfeolu@gmail.com", 1234);
+  var sampleAdmin1 = new Admin("Adedayo", "samfeolu@gmail.com", 1234);
   expect(sampleAdmin1.password).toBe(1234);
+});
+
+test("Should Add First User Into The DataBase WIth Name Shalom", function() {
+  expect(db.users["1"].name).toBe("Shalom");
+});
+
+test("Should Add Second User Into The DataBase With Name Dickson", function() {
+  expect(db.users["2"].name).toBe("Dickson");
+});
+
+test("Should Add First User Into The DataBase WIth email 'shalom@gmail.com'", function() {
+  expect(db.users["1"].email).toBe("shalom@gmail.com");
+});
+
+test("Should Add Second User Into The DataBase With email 'dickson@gmail.com'", function() {
+  expect(db.users["2"].email).toBe("dickson@gmail.com");
 });
