70019c609b0a8f5e2d50bc78d93bcb5fd0cbd9af
Techkny <bobadeprojects@gmail.com>
2019-04-17 12:51:51 +0100

    Added Test To Check For Type Of User Both In The Object And In The Database

--- app.test.js
+++ app.test.js
@@ -1,24 +1,24 @@
 const { User } = require("./app");
 const { Admin } = require("./app");
 const db = require("./database");
+var Shalom = new User("Shalom", "shalom@gmail.com", 12345);
+var Dickson = new User("Dickson", "dickson@gmail.com", 12345);
+var Uchenna = new User("Uchenna", "shalom@gmail.com", 12345);
+var Adedayo = new Admin("Adedayo", "samfeolu@gmail.com", 1234);
 
 test('Should Create New User with corresponding name "Shalom" ', function() {
-  var Shalom = new User("Shalom", "shalom@gmail.com", 12345);
   expect(Shalom.name).toBe("Shalom");
 });
 
 test('Should Create New User with corresponding email "shalom@gmail.com" ', function() {
-  var Dickson = new User("Dickson", "dickson@gmail.com", 12345);
   expect(Dickson.email).toBe("dickson@gmail.com");
 });
 
 test('Should Create New User with corresponding password "12345" ', function() {
-  var Uchenna = new User("Uchenna", "shalom@gmail.com", 12345);
   expect(Uchenna.password).toBe(12345);
 });
 
 test('Should Create New Admin with corresponding name "Adedayo" ', function() {
-  var Adedayo = new Admin("Adedayo", "samfeolu@gmail.com", 1234);
   expect(Adedayo.name).toBe("Adedayo");
 });
 
@@ -61,7 +61,38 @@ test("Should Add First Admin Into The DataBase WIth email 'samfeolu@gmail.com'",
 });
 
 test("Should Add Second User Into The DataBase With email 'samfeolu@gmail.com'", function() {
-  console.log(db);
-
   expect(db.users["5"].email).toBe("samfeolu@gmail.com");
+});
+
+test("Check For The Type Of User In The Database should return 'Admin' ", function() {
+  expect(db.users["5"].type).toBe("Admin");
+});
+
+test("Check For The Type Of User In The Database should return 'Admin' ", function() {
+  expect(db.users["6"].type).toBe("Admin");
+});
+
+test("Check For The Type Of User In The Database should return 'User' ", function() {
+  expect(db.users["1"].type).toBe("User");
+});
+
+test("Check For The Type Of User In The Database should return 'User' ", function() {
+  expect(db.users["6"].type).toBe("User");
+});
+
+test("Check For The Type Of User In The Shalom Object should return 'User' ", function() {
+  expect(Shalom.type).toBe("User");
+});
+
+test("Check For The Type Of User In The Uchenna Object should return 'User' ", function() {
+  expect(Uchenna.type).toBe("User");
+});
+
+test("Check For The Type Of User In The sampleAdmin Object should return 'Admin' ", function() {
+  expect(sampleAdmin.type).toBe("Admin");
+});
+
+test("Check For The Type Of User In The sampleAdmin1 Object should return 'Admin' ", function() {
+  console.log(db);
+  expect(sampleAdmin1.type).toBe("Admin");
 });
