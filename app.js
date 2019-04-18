045a31de830bd4f207825003d49a673a11d69693
Techkny <bobadeprojects@gmail.com>
2019-04-18 13:26:16 +0100

    Refactored All Test To Pass All Test

--- app.js
+++ app.js
@@ -1,67 +1,69 @@
 "use strict";
+//May The Code Be With You In Jesus Name
+//May The Code Be With You In Jesus Name
 const db = require("./database");
 
 function User(name, email, password) {
-  //May The Code Be With You In Jesus Name
-
-  this.name = name;
-  this.email = email;
-  this.password = password;
-  if (this.constructor == User) {
-    this.type = "User";
-  } else if (this.constructor == Admin) {
-    this.type = "Admin";
-  }
-  for (var i = 1; i >= 0; ) {
-    if (!db.users[i + ""]) {
-      this.id = i + "";
-      db.users[i + ""] = {
-        name: this.name,
-        email: this.email,
-        password: this.password,
-        type: this.type,
-        id: this.id
-      };
-
-      break;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    if (this.constructor == User) {
+        this.type = "User";
+    } else if (this.constructor == Admin) {
+        this.type = "Admin";
     }
-    i++;
-  }
+    for (var i = 1; i >= 0; ) {
+        if (!db.users[i + ""]) {
+            this.id = i + "";
+            db.users[i + ""] = {
+                name: this.name,
+                email: this.email,
+                password: this.password,
+                type: this.type,
+                id: this.id
+            };
 
-  console.log(this);
+            break;
+        }
+        i++;
+    }
+
+    console.log(this);
 }
 User.prototype.readSingleUser = function(id) {
-  return db.users[id.toString()];
+    return db.users[id.toString()];
 };
 User.prototype.updateSingleUser = function(id, name, email, password) {
-  if (!id || !name || !email || !password) {
-    return false;
-  }
-  if (id.toString() !== this.id) {
-    return false;
-  }
-  db.users[id.toString()].name = name;
-  db.users[id.toString()].email = email;
-  db.users[id.toString()].password = password;
+    if (!id || !name || !email || !password) {
+        return false;
+    }
+    if (id.toString() !== this.id) {
+        return false;
+    }
+    db.users[id.toString()].name = name;
+    db.users[id.toString()].email = email;
+    db.users[id.toString()].password = password;
 
-  return true;
+    return true;
 };
 User.prototype.searchByName = function(name) {
-  for (var user in db.users) {
-    if (db.users[user].name == name) {
-      return db.users[user];
+    for (var user in db.users) {
+        if (db.users[user].name == name) {
+            return db.users[user];
+        }
     }
-  }
+    return false;
 };
-function Admin(name, email, password) {
-  //May The Code Be With You In Jesus Name
 
-  User.call(this, name, email, password);
+//Admin Functions
+function Admin(name, email, password) {
+    User.call(this, name, email, password);
 }
 Admin.prototype = Object.create(User.prototype, {
-  constructor: { value: Admin }
+    constructor: { value: Admin }
 });
+
 module.exports = {
-  User,
-  Admin
+    User,
+    Admin
 };
--- app.test.js
+++ app.test.js
@@ -9,136 +9,140 @@ var sampleAdmin = new Admin("Adedunye", "samfeolu@gmail.com", 1234);
 var sampleAdmin1 = new Admin("Adedayo", "samfeolu@gmail.com", 1234);
 
 test('Should Create New User with corresponding name "Shalom" ', function() {
-  expect(Shalom.name).toBe("Shalom");
+    expect(Shalom.name).toBe("Shalom");
 });
 
 test('Should Create New User with corresponding email "shalom@gmail.com" ', function() {
-  expect(Dickson.email).toBe("dickson@gmail.com");
+    expect(Dickson.email).toBe("dickson@gmail.com");
 });
 
 test('Should Create New User with corresponding password "12345" ', function() {
-  expect(Uchenna.password).toBe(12345);
+    expect(Uchenna.password).toBe(12345);
 });
 
 test('Should Create New Admin with corresponding name "Adedayo" ', function() {
-  expect(Adedayo.name).toBe("Adedayo");
+    expect(Adedayo.name).toBe("Adedayo");
 });
 
 test('Should Create New Admin with corresponding email "samfeolu@gmail.com" ', function() {
-  expect(sampleAdmin.email).toBe("samfeolu@gmail.com");
+    expect(sampleAdmin.email).toBe("samfeolu@gmail.com");
 });
 
 test('Should Create New User with corresponding password "12345" ', function() {
-  expect(sampleAdmin1.password).toBe(1234);
+    expect(sampleAdmin1.password).toBe(1234);
 });
 
 test("Should Add First User Into The DataBase WIth Name Shalom", function() {
-  expect(db.users["1"].name).toBe("Shalom");
+    expect(db.users["1"].name).toBe("Shalom");
 });
 
 test("Should Add Second User Into The DataBase With Name Dickson", function() {
-  expect(db.users["2"].name).toBe("Dickson");
+    expect(db.users["2"].name).toBe("Dickson");
 });
 
 test("Should Add First User Into The DataBase WIth email 'shalom@gmail.com'", function() {
-  expect(db.users["1"].email).toBe("shalom@gmail.com");
+    expect(db.users["1"].email).toBe("shalom@gmail.com");
 });
 
 test("Should Add Second User Into The DataBase With email 'dickson@gmail.com'", function() {
-  expect(db.users["2"].email).toBe("dickson@gmail.com");
+    expect(db.users["2"].email).toBe("dickson@gmail.com");
 });
 
 test("Should Add First Admin Into The DataBase WIth Name Adedayo", function() {
-  expect(db.users["4"].name).toBe("Adedayo");
+    expect(db.users["4"].name).toBe("Adedayo");
 });
 
 test("Should Add Second Admin Into The DataBase With Name Adedunye", function() {
-  expect(db.users["5"].name).toBe("Adedunye");
+    expect(db.users["5"].name).toBe("Adedunye");
 });
 
 test("Should Add First Admin Into The DataBase WIth email 'samfeolu@gmail.com'", function() {
-  expect(db.users["4"].email).toBe("samfeolu@gmail.com");
+    expect(db.users["4"].email).toBe("samfeolu@gmail.com");
 });
 
 test("Should Add Second User Into The DataBase With email 'samfeolu@gmail.com'", function() {
-  expect(db.users["5"].email).toBe("samfeolu@gmail.com");
+    expect(db.users["5"].email).toBe("samfeolu@gmail.com");
 });
 
 test("Check For The Type Of User In The Database should return 'Admin' ", function() {
-  expect(db.users["5"].type).toBe("Admin");
+    expect(db.users["5"].type).toBe("Admin");
 });
 
 test("Check For The Type Of User In The Database should return 'Admin' ", function() {
-  expect(db.users["6"].type).toBe("Admin");
+    expect(db.users["6"].type).toBe("Admin");
 });
 
 test("Check For The Type Of User In The Database should return 'User' ", function() {
-  expect(db.users["1"].type).toBe("User");
+    expect(db.users["1"].type).toBe("User");
 });
 
 test("Check For The Type Of User In The Database should return 'User' ", function() {
-  expect(db.users["3"].type).toBe("User");
+    expect(db.users["3"].type).toBe("User");
 });
 
 test("Check For The Type Of User In The Shalom Object should return 'User' ", function() {
-  expect(Shalom.type).toBe("User");
+    expect(Shalom.type).toBe("User");
 });
 
 test("Check For The Type Of User In The Uchenna Object should return 'User' ", function() {
-  expect(Uchenna.type).toBe("User");
+    expect(Uchenna.type).toBe("User");
 });
 
 test("Check For The Type Of User In The sampleAdmin Object should return 'Admin' ", function() {
-  expect(sampleAdmin.type).toBe("Admin");
+    expect(sampleAdmin.type).toBe("Admin");
 });
 
 test("Check For The Type Of User In The sampleAdmin1 Object should return 'Admin' ", function() {
-  console.log(db);
-  expect(sampleAdmin1.type).toBe("Admin");
+    console.log(db);
+    expect(sampleAdmin1.type).toBe("Admin");
 });
 
 test("Read User By Id From a User Should Return Dickson Object", function() {
-  expect(Dickson.readSingleUser(2)).toEqual(Dickson);
+    expect(Dickson.readSingleUser(2)).toEqual(Dickson);
 });
 
 test("Read User By Id From a User Should Return Dickson Object", function() {
-  expect(Uchenna.readSingleUser(1)).toEqual(Shalom);
+    expect(Uchenna.readSingleUser(1)).toEqual(Shalom);
 });
 
 test("Read User By Id From An Admin Should Return Dickson Object", function() {
-  expect(Adedayo.readSingleUser(1)).toEqual(Shalom);
+    expect(Adedayo.readSingleUser(1)).toEqual(Shalom);
 });
 
 test("Update Single User should only work for self", function() {
-  expect(
-    Adedayo.updateSingleUser(1, "Bobby", "bobade@gmail.com", 1234)
-  ).toBeFalsy();
+    expect(
+        Adedayo.updateSingleUser(1, "Bobby", "bobade@gmail.com", 1234)
+    ).toBeFalsy();
 });
 
 test("Update Single User should not work for others and should not affect database", function() {
-  expect(db.users["1"].email).toBe("shalom@gmail.com");
+    expect(db.users["1"].email).toBe("shalom@gmail.com");
 });
 
 test("Update Single User should update user details", function() {
-  expect(
-    Adedayo.updateSingleUser(4, "Bobby", "bobade@gmail.com", 1234)
-  ).toBeTruthy();
+    expect(
+        Adedayo.updateSingleUser(4, "Bobby", "bobade@gmail.com", 1234)
+    ).toBeTruthy();
 });
 
 test("Update Single User should work for self and should affect database", function() {
-  expect(db.users["4"].email).toBe("bobade@gmail.com");
+    expect(db.users["4"].email).toBe("bobade@gmail.com");
 });
 
 test("Update Single User should work for self and should affect database", function() {
-  expect(db.users["4"].name).toBe("Bobby");
+    expect(db.users["4"].name).toBe("Bobby");
 });
 
 test("Update Single User should have Completed parameters", function() {
-  expect(
-    Adedayo.updateSingleUser("Bobby", "bobade@gmail.com", 1234)
-  ).toBeFalsy();
+    expect(
+        Adedayo.updateSingleUser("Bobby", "bobade@gmail.com", 1234)
+    ).toBeFalsy();
 });
 
 test("Search User by Name", function() {
-  expect(Adedayo.searchByName("Adedayo")).toEqual(sampleAdmin1);
+    expect(Adedayo.searchByName("Adedayo")).toEqual(sampleAdmin1);
+});
+
+test("Search User by Name", function() {
+    expect(Adedayo.searchByName("uchenna")).toBe(false);
 });
